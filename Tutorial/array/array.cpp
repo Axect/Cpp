417de8c08f70890338a08b8eea395a0412d07d6a
Axect <edeftg@gmail.com>
2017-09-24 21:37:25 +0900

    Success Array!

---
+++ Tutorial/array/array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+    clock_t begin, end;
+    begin = clock();
+
+    int * S = new int[100000000];
+    long s = 0;
+
+    for (int i; i < 1E+08; i++) {
+        S[i] = i + 1;
+    }
+    for (int i; i < 1E+08; i++) {
+        s += S[i];
+    }
+
+    delete []S;
+
+    end = clock();
+    long msec = end - begin;
+
+    cout << s << endl;
+    cout << msec/1000 << "ms" << endl;
+
+    return 0;
+}
